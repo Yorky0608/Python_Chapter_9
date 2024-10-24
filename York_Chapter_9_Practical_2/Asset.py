45f8d4bd45178e7888138fa26d3f317212e5e790
Caleb York <caleb.york@fwcsstudents.org>
2024-10-24 11:48:44 -0400

    Delete York_Chapter_9_Practical_2 directory

--- York_Chapter_9_Practical_2/Asset.py
+++
@@ -1,8 +0,0 @@
-class Asset:
-
-    """Creating variable name, address, and _revenue <---(protected but not private)"""
-
-    def __init__(self, name, address, rent=0):
-        self.name = name
-        self.address = address
-        self._revenue = float(rent)
